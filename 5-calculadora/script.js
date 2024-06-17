456209be1a596cbafad30c42064d50fe4bf3ce89
Isaque Nogueira <isaquenogueira102@gmail.com>
2024-06-16 22:13:10 -0300

    Adicionar projeto calculadora

---
+++ 5-calculadora/script.js
@@ -0,0 +1,28 @@
+const visor = document.querySelector("#visor");
+const botoes = document.querySelectorAll(".button");
+const resultado = document.querySelector("#igual");
+const ce = document.querySelector("#ce");
+const limpar = document.querySelector("#limpar");
+
+
+
+botoes.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        visor.value += botao.textContent;
+    })
+})
+
+resultado.addEventListener("click", () => {
+    let conta = visor.value;
+    conta = eval(conta);
+    visor.value = conta;
+})
+
+ce.addEventListener("click", () => {
+    visor.value = "";
+})
+
+limpar.addEventListener("click", () => {
+
+    visor.value = visor.value.substring(0, visor.value.length - 1)
+})
