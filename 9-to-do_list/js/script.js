2c1c9384e8d7d397a2b054a2ece98dd23a0cba05
Isaque Nogueira <isaquenogueira102@gmail.com>
2024-07-03 17:49:00 -0300

    melhoria to do list

--- 9-to-do_list/js/script.js
+++ 9-to-do_list/js/script.js
@@ -52,6 +52,7 @@ addTarefa.addEventListener("click", () => {
     let tarefaDado = campoInput.value;
     campoInput.value = "";
     addTarefa.disabled = true;
+    campoInput.blur();
     renderizarTarefas(tarefaDado);
 })
 
@@ -68,68 +69,115 @@ function renderizarTarefas(dado){
     containerTarefa.classList.add("containerTarefa");
     marcadorTarefa.classList.add("marcador");
     tarefa.classList.add("tarefa-criada");
-    opcoesTarefa.classList.add("opcoes");
-    opcoesTarefa.classList.add("fa-solid");
-    opcoesTarefa.classList.add("fa-ellipsis");
+    opcoesTarefa.classList.add("opcoes", "fa-solid", "fa-ellipsis");
     tarefa.value = dado;
-
-    // caixa opções
-
-    let caixaOpcao = document.createElement("div");
-    let cores = document.createElement("div");
-    let cinza = document.createElement("spas");
-    let azul = document.createElement("span");
-    let gold = document.createElement("span");
-    let vermelho = document.createElement("span");
-    let editar = document.createElement("span");
-    let deletar = document.createElement("span");
-
-    caixaOpcao.classList.add("caixaOpcoes");
-    cinza.classList.add("cor", "cinza");
-    azul.classList.add("cor", "azul");
-    gold.classList.add("cor", "gold");
-    vermelho.classList.add("cor", "vermelho");
-    cores.classList.add("cores");
-    editar.classList.add("editarTarefa");
-    deletar.classList.add("deletarTarefa");
-
-    cores.appendChild(cinza)
-    cores.appendChild(azul)
-    cores.appendChild(gold)
-    cores.appendChild(vermelho)
-    editar.textContent = "Editar";
-    deletar.textContent = "Deletar";
-    caixaOpcao.appendChild(cores);
-    caixaOpcao.appendChild(editar);
-    caixaOpcao.appendChild(deletar);
+    tarefa.disabled = true;
 
     // adicionar aos elementos pais
     containerTarefa.appendChild(marcadorTarefa);
     containerTarefa.appendChild(tarefa);
     containerTarefa.appendChild(opcoesTarefa);
-    containerTarefa.appendChild(caixaOpcao);
     divTarefas.appendChild(containerTarefa);
 
-    marcadorTarefa.addEventListener("click", () => {tarefaConcluida(containerTarefa, marcadorTarefa, tarefa, opcoesTarefa)});
-    opcoesTarefa.addEventListener("click", () => {caixaOpcoes(containerTarefa, tarefa, opcoesTarefa, caixaOpcao, cinza, vermelho, azul, gold)})
+    criarCaixaOpcoes(containerTarefa, tarefa, opcoesTarefa, marcadorTarefa)
 }
 
-function tarefaConcluida(container, marcador, tarefa, opcoes){
+function criarCaixaOpcoes(containerTarefa, tarefa, opcoesTarefa, marcadorTarefa) {
+
+        let caixaOpcao = document.createElement("div");
+        let cores = document.createElement("div");
+        let cinza = document.createElement("spas");
+        let azul = document.createElement("span");
+        let gold = document.createElement("span");
+        let vermelho = document.createElement("span");
+        let editar = document.createElement("span");
+        let deletar = document.createElement("span");
+    
+        caixaOpcao.classList.add("caixaOpcoes");
+        cinza.classList.add("cor", "cinza");
+        azul.classList.add("cor", "azul");
+        gold.classList.add("cor", "gold");
+        vermelho.classList.add("cor", "vermelho");
+        cores.classList.add("cores");
+        editar.classList.add("editarTarefa");
+        deletar.classList.add("deletarTarefa");
+    
+        cores.appendChild(cinza)
+        cores.appendChild(azul)
+        cores.appendChild(gold)
+        cores.appendChild(vermelho)
+        editar.textContent = "Editar";
+        deletar.textContent = "Deletar";
+        caixaOpcao.appendChild(cores);
+        caixaOpcao.appendChild(editar);
+        caixaOpcao.appendChild(deletar);
+        containerTarefa.appendChild(caixaOpcao);
+
+        opcoesTarefa.addEventListener("click", () => {caixaOpcoes(containerTarefa, tarefa, opcoesTarefa, caixaOpcao, cinza, vermelho, azul, gold, editar, deletar)})
+        marcadorTarefa.addEventListener("click", () => {tarefaConcluida(containerTarefa, marcadorTarefa, tarefa, opcoesTarefa, caixaOpcao)});
+
+}
+
+function caixaOpcoes(container, tarefa, opcoes, caixaOpcao, cinza, vermelho, azul, gold, editar, deletar){
+
+    caixaOpcao.classList.toggle("mostrar")
+    cinza.addEventListener("click", () => {mudarCor(container, "cinza")})
+    vermelho.addEventListener("click", () => {mudarCor(container, "vermelho")})
+    azul.addEventListener("click", () => {mudarCor(container, "azul")})
+    gold.addEventListener("click", () => {mudarCor(container, "gold")})
+    editar.addEventListener("click", () => {editarTarefa(container, tarefa, caixaOpcao)})
+    deletar.addEventListener("click", () => {deletarTarefa(container, caixaOpcao)})
+
+    cliqueForaArea(caixaOpcao, opcoes, tarefa)
+}
+
+function cliqueForaArea(caixaOpcao, opcoes, tarefa){
+
+    document.addEventListener("click", (event) => {
+        if(!caixaOpcao.contains(event.target) && !opcoes.contains(event.target)){
+            caixaOpcao.classList.remove("mostrar")
+        }
+
+        if(tarefa.disabled === false && !tarefa.contains(event.target)){
+            tarefa.disabled = true;
+        }
+
+        })
+
+}
+
+
+function tarefaConcluida(container, marcador, tarefa, opcoes, caixaOpcao){
     
     container.classList.toggle("concluidaContainer")
     marcador.classList.toggle("concluidaMarcador")
     tarefa.disabled = true;
     tarefa.classList.toggle("concluidaTarefa")
-    opcoes.classList.add("esconder")
+    opcoes.classList.toggle("esconder")
+    caixaOpcao.classList.remove("mostrar")
 }
 
-function caixaOpcoes(container, tarefa, opcoes, caixaOpcao, cinza, vermelho, azul, gold){
 
-    caixaOpcao.classList.toggle("mostrar");
-    cinza.addEventListener("click", () => {mudarCor(container, "cinza")})
-    vermelho.addEventListener("click", () => {mudarCor(container, "vermelho")})
-    azul.addEventListener("click", () => {mudarCor(container, "azul")})
-    gold.addEventListener("click", () => {mudarCor(container, "gold")})
+function editarTarefa(container, tarefa, caixaOpcao){
+
+    setTimeout(() => {    
+        tarefa.disabled = false;
+        tarefa.focus();
+    }, 100)
+
+    document.addEventListener("keydown", (event) => {
+        if(event.key === "Enter" && tarefa.disabled === false){
+            tarefa.disabled = true;
+        }
+    })
+    caixaOpcao.classList.remove("mostrar");
+}
+
+function deletarTarefa(container, caixaOpcao){
+
+    setTimeout(() => {
+        container.remove();
+    }, 100)
 }
 
 // funções secundárias 
